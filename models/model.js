c0716a15810a83309142df03ae99eeed3a27975c
caolanhamilton <caolanhamilton75@gmail.com>
2022-05-16 14:57:45 +0100

    add fecthArticleByID model function

--- models/model.js
+++ models/model.js
@@ -2,7 +2,19 @@ const res = require("express/lib/response")
 const db = require("../db/connection.js")
 
 exports.fetchTopics = () => {
-    return db.query("SELECT * FROM topics").then((res) => {
-        return res.rows;
+    return db
+        .query("SELECT * FROM topics").then((response) => {
+        return response.rows;
     })
+}
+
+exports.fetchArticleByID = (articleID) => {
+    return db
+        .query("SELECT * FROM articles WHERE article_id = $1", [articleID])
+        .then((response) => {
+            if (response.rowCount === 0) {
+                return Promise.reject({status: 404, msg: "Resource not found with this ID"})
+            }
+            return response.rows[0]
+        });
 }
