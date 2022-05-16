6c7af3aad42476e3db64f7efd48224ec8902e32f
caolanhamilton <caolanhamilton75@gmail.com>
2022-05-16 14:58:29 +0100

    add /api/articles/:article_id endpoint

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const { getTopics } = require("./controllers/controller")
+const { getTopics, getArticleByID } = require("./controllers/controller")
 
 const app = express();
 app.use(express.json());
@@ -7,10 +7,28 @@ app.use(express.json());
 //GET
 
 app.get("/api/topics", getTopics);
+app.get("/api/articles/:article_id", getArticleByID);
 
 //ERROR HANDLING
 app.use("/*", (req, res, next) => {
     res.status(404).send({ msg: "Path not found" });
   });
+
+app.use((err, req, res, next) => { //deal with PSQL error
+    if (err.code === '22P02') {
+      res.status(400).send({msg: "Invalid ID type"});
+    } else {
+      next(err);
+    }
+});
+
+app.use((err, req, res, next) => { //deal with customer errors
+    if (err.status){
+      res.status(err.status).send({msg: err.msg})
+    } else {
+      next(err)
+    }
+})
+
 
 module.exports = app;
