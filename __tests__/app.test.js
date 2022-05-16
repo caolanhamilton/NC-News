9da3d9b694e032b402416dc62fca20f745f960fb
caolanhamilton <caolanhamilton75@gmail.com>
2022-05-16 11:55:12 +0100

    add test suite

---
+++ __tests__/app.test.js
@@ -0,0 +1,37 @@
+process.env.NODE_ENV = 'test';
+const seed = require("../db/seeds/seed.js")
+const testData = require("../db/data/test-data/index.js")
+const db = require("../db/connection.js")
+const app = require("../app.js")
+const request = require("supertest")
+
+afterAll(() => db.end());
+
+beforeEach(() => seed(testData));
+
+describe('GET /api/topics', () => {
+    test('200: Returns array of topic objects ', () => {
+    return request(app)
+    .get("/api/topics")
+    .expect(200)
+    .then(({body}) => {
+        const arrayOfTopicsObjs = body.topics
+        expect(arrayOfTopicsObjs).toHaveLength(3);
+        expect(arrayOfTopicsObjs).toBeInstanceOf(Array)
+        arrayOfTopicsObjs.forEach((topicObj) => {
+            expect(topicObj).toMatchObject({
+                description: expect.any(String),
+                slug: expect.any(String)
+            })
+        })
+      })
+    });
+    test('404: End-point not found ', () => {
+      return request(app)
+      .get("/api/notapath")
+      .expect(404)
+      .then(({body}) => {
+        expect(body).toEqual({ msg: "Path not found" });
+      });
+    });
+});
