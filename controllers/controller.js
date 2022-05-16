6b62998bf783693f69aee10c72897d23515a1d10
caolanhamilton <caolanhamilton75@gmail.com>
2022-05-16 14:57:14 +0100

    add getArticleByID controller function

--- controllers/controller.js
+++ controllers/controller.js
@@ -1,9 +1,23 @@
-const {fetchTopics} = require("../models/model.js")
+const { response } = require("../app.js");
+const {fetchTopics, fetchArticleByID} = require("../models/model.js")
+
 exports.getTopics = (req, res, next) => {
     fetchTopics()
         .then((arrayOfTopicsObjs) => {
             res.status(200).send({topics: arrayOfTopicsObjs})
         }).catch((err) => {
             next(err)
+        });
+}
+
+exports.getArticleByID = (req, res, next) => {
+    const articleID = req.params.article_id
+    fetchArticleByID(articleID)
+        .then((articleObj) => {
+            res.status(200).send({articleObj})
+        })
+        .catch((err)=> {
+            console.log(err)
+            next(err);
         });
 }
