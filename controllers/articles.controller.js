999d0a2e94062b22785d1d55d711958ed1ce8da3
caolanhamilton <caolanhamilton75@gmail.com>
2022-05-19 09:12:27 +0100

    Add sort, order, topic as parameters to fecthAllArticles from req.query

--- controllers/articles.controller.js
+++ controllers/articles.controller.js
@@ -12,14 +12,15 @@ exports.getArticleByID = (req, res, next) => {
 }
 
 exports.getAllArticles = (req, res, next) => {
-    fetchAllArticles()
+    const {sort_by, order, topic} = req.query
+    fetchAllArticles(sort_by, order, topic)
         .then((articles) => {
             res.status(200).send({articles})
         })
         .catch((err)=> {
             next(err)
         })
-}
+ }
 
 exports.patchArticleVotes = (req, res, next) => {
     const articleID = req.params.article_id
