931381923df950cbc0b1692cc8c04d8114d86154
caolanhamilton <caolanhamilton75@gmail.com>
2022-05-20 12:17:14 +0100

    update module.exports line

--- db/connection.js
+++ db/connection.js
@@ -10,7 +10,7 @@ const config =
       }
     : {};
 
-module.exports = new Pool(config);
+
 
 require('dotenv').config({
   path: `${__dirname}/../.env.${ENV}`,
@@ -20,4 +20,4 @@ if (!process.env.PGDATABASE && !process.env.DATABASE_URL) {
   throw new Error('PGDATABASE or DATABASE_URL not set');
 }
 
-module.exports = new Pool();
+module.exports = new Pool(config);
